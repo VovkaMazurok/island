69f7a469b281f5bafab662703d393969e3b34587
Volodymyre <vova89.89@i.ua>
2024-01-20 12:49:26 +0200

    add

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ import org.example.Entity.JsonFile;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        JsonFile.readJsonFile();
+        JsonFile.record();
 
     }
 }
